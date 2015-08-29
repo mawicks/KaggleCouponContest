5fd971b239c947f926589423d8a759b31ec314ff
Mark Wicks <mawicks@gmail.com>
2015-08-29 10:30:41 -0400

    Improve variable naming in naive_bayes.py

--- naive_bayes.py
+++ naive_bayes.py
@@ -31,12 +31,11 @@ class Estimator:
            to accumulate stats on"""
 
         self.strategy = strategy
-        self.known_field_values = set()
+        self.known_attribute_values = set()
 
-        self.item_count_by_field_value = {}
-        self.item_count = 0
-
-        self.count_by_field_value_and_earlier_field_value = {}
+        self.class_counts = {}
+        self.count_by_class_and_attribute = {}
+        self.class_count = 0
 
         self.column_count = {}
         self.row_count = {}
@@ -45,33 +44,33 @@ class Estimator:
     def __repr__ (self):
         return "Accumulator({0}".format(self.strategy)
 
-    def add (self, field_value, history_set):
-        self.item_count_by_field_value[field_value] = self.item_count_by_field_value.get(field_value, 0) + 1
-        self.item_count += 1
+    def add (self, class_value, history_set):
+        self.class_counts[class_value] = self.class_counts.get(class_value, 0) + 1
+        self.class_count += 1
 
-        for historic_field_value,count in history_set.items():
-            self.known_field_values.add(historic_field_value)
+        for historic_attribute_value,count in history_set.items():
+            self.known_attribute_values.add(historic_attribute_value)
             count = self.strategy.occurences(count)
 
-            t = (field_value, historic_field_value)
-            self.count_by_field_value_and_earlier_field_value[t] = self.count_by_field_value_and_earlier_field_value.get(t, 0) + count
+            t = (class_value, historic_attribute_value)
+            self.count_by_class_and_attribute[t] = self.count_by_class_and_attribute.get(t, 0) + count
 
-            self.column_count[historic_field_value] = self.column_count.get(historic_field_value, 0) + count
-            self.row_count[field_value] = self.row_count.get(field_value, 0) + count
+            self.column_count[historic_attribute_value] = self.column_count.get(historic_attribute_value, 0) + count
+            self.row_count[class_value] = self.row_count.get(class_value, 0) + count
             self.total_count += count
 
     def dump (self, limit=None):
         if limit:
             print ('Limited to {0} category values'.format(limit))
 
-        field_values = list(self.known_field_values)[0:limit]
+        field_values = list(self.known_attribute_values)[0:limit]
         print('value\n\t\tall {0}'.format(' '.join(map('{0:>10}'.format, field_values))))
 
         for fv in field_values:
             print ('{0:>10}:\n\t{1:>10} {2}'.format(
                 fv,
                 self.row_count.get(fv, 0),
-                ' '.join(map('{0:>10}'.format, [self.count_by_field_value_and_earlier_field_value.get((fv,pv), 0) for pv in field_values]))
+                ' '.join(map('{0:>10}'.format, [self.count_by_class_and_attribute.get((fv,pv), 0) for pv in field_values]))
             ))
 
         print ('column sums:\n\t{0:>10} {1}'.format(
@@ -80,27 +79,27 @@ class Estimator:
         ))
 
         print ('item counts:\n\t{0:>10} {1}'.format(
-            self.item_count,
-            ' '.join(map('{0:>10}'.format, (self.item_count_by_field_value.get(fv,0) for fv in field_values[0:limit])))
+            self.class_count,
+            ' '.join(map('{0:>10}'.format, (self.class_counts.get(fv,0) for fv in field_values[0:limit])))
         ))
 
-    def score (self, candidate_field_value, history_set):
-        N = len(self.known_field_values)
+    def score (self, candidate_class_value, history_set):
+        N = len(self.known_attribute_values)
 
-        candidate_item_count = self.item_count_by_field_value.get(candidate_field_value, 0)
-        not_candidate_item_count = self.item_count - candidate_item_count
+        candidate_class_count = self.class_counts.get(candidate_class_value, 0)
+        not_candidate_class_count = self.class_count - candidate_class_count
 
-        p_class = float(1.0 + candidate_item_count) / (N + self.item_count)
+        p_class = float(1.0 + candidate_class_count) / (N + self.class_count)
         p_not_class = 1.0 - p_class
 
         log_likelihood = math.log(p_class / p_not_class)
 
-        candidate_row_count = self.row_count.get(candidate_field_value, 0)
+        candidate_row_count = self.row_count.get(candidate_class_value, 0)
 
-        for fv in self.known_field_values:
-            t = (candidate_field_value, fv)
+        for fv in self.known_attribute_values:
+            t = (candidate_class_value, fv)
 
-            field_value_count = self.count_by_field_value_and_earlier_field_value.get(t, 0)
+            field_value_count = self.count_by_class_and_attribute.get(t, 0)
 
             p_x_candidate = float(1.0 + field_value_count) / (N + candidate_row_count)
             p_x_not_candidate = float(1.0 + self.column_count.get(fv, 0) - field_value_count) / (N + self.total_count - candidate_row_count)
